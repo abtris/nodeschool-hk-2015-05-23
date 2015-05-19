2ee04ca90066db08fabfa6ee10b1317aa042596f
Ladislav Prskavec <ladislav@prskavec.net>
2015-05-19 14:16:26 +0200

    Fix dredd hook file

--- koa-dredd-hook.js
+++ koa-dredd-hook.js
@@ -1,7 +1,22 @@
 var hooks = require('hooks');
 var before = hooks.before;
 
-hooks.beforeEach(function(transaction, done) {
-  transaction.response.headers['content-type'] = 'application/json; charset=utf-8';
+hooks.before('Polls API Root > Retrieve the Entry Point', function(transaction, done) {
+  transaction.expected.headers['Content-Type'] = 'application/json; charset=utf-8';
+  return done();
+});
+
+hooks.before('Question > Question > View a Questions Detail', function(transaction, done) {
+  transaction.expected.headers['Content-Type'] = 'application/json; charset=utf-8';
+  return done();
+});
+
+hooks.before('Question > Questions Collection > List All Questions', function(transaction, done) {
+  transaction.expected.headers['Content-Type'] = 'application/json; charset=utf-8';
+  return done();
+});
+
+hooks.before('Question > Questions Collection > Create a New Question', function(transaction, done) {
+  transaction.expected.headers['Content-Type'] = 'application/json; charset=utf-8';
   return done();
 });
